3a59bb0e8b78841e95018a4cad3cabf449cf8046
Kittipong <anonymous51124@gamil.com>
2025-01-31 23:26:14 +0700

    lab15_5 complete

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,29 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+
+void shuffle(int *x, int *y, int *z, int *w) {
+    
+    int randIndex1 = rand() % 4;  
+    int randIndex2 = rand() % 4;  
+    
+    
+    while (randIndex1 == randIndex2) {
+        randIndex2 = rand() % 4;  
+    }
+
+    
+    if (randIndex1 == 0 && randIndex2 == 1) {
+        swap(*x, *y);  
+    } else if (randIndex1 == 0 && randIndex2 == 2) {
+        swap(*x, *z);  
+    } else if (randIndex1 == 0 && randIndex2 == 3) {
+        swap(*x, *w);  
+    } else if (randIndex1 == 1 && randIndex2 == 2) {
+        swap(*y, *z);  
+    } else if (randIndex1 == 1 && randIndex2 == 3) {
+        swap(*y, *w);  
+    } else if (randIndex1 == 2 && randIndex2 == 3) {
+        swap(*z, *w);  
+    }
+}
