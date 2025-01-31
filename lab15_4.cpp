54e624a99e86e888b5a3970f5646bfe2c6af7695
Kittipong <anonymous51124@gamil.com>
2025-01-31 23:26:07 +0700

    lab15_4 complete

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,28 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &x, int &y, int &z, int &w) {
+    
+    int randIndex1 = rand() % 4;  
+    int randIndex2 = rand() % 4;  
+    
+    
+    while (randIndex1 == randIndex2) {
+        randIndex2 = rand() % 4;
+    }
+
+  
+    if (randIndex1 == 0 && randIndex2 == 1) {
+        swap(x, y);  
+    } else if (randIndex1 == 0 && randIndex2 == 2) {
+        swap(x, z);  
+    } else if (randIndex1 == 0 && randIndex2 == 3) {
+        swap(x, w); 
+    } else if (randIndex1 == 1 && randIndex2 == 2) {
+        swap(y, z);  
+    } else if (randIndex1 == 1 && randIndex2 == 3) {
+        swap(y, w);  
+    } else if (randIndex1 == 2 && randIndex2 == 3) {
+        swap(z, w);  
+    }
+}
